dde977fed093223c0f255b3683374104cb2e8ffa
Nodar Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-04-04 22:39:11 +0400

    remove ads from every page

--- userscripts/scripts/piratebay-adfree.user.js
+++ userscripts/scripts/piratebay-adfree.user.js
@@ -4,9 +4,9 @@
 // @namespace   nod
 // @downloadURL https://nodar-chkuaselidze.github.io/userscripts/scripts/piratebay-adfree.user.js
 // @updateURL   https://nodar-chkuaselidze.github.io/userscripts/scripts/piratebay-adfree.meta.js
-// @include     http://thepiratebay.se/search/*
-// @include     https://thepiratebay.se/search/*
-// @version     1.0.0
+// @include     http://thepiratebay.se/*
+// @include     https://thepiratebay.se/*
+// @version     1.0.1
 // @grant       none
 // ==/UserScript==
 
