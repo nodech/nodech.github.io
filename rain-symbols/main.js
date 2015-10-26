ac10b0b64dbe4952ea86efa50c54458f8e195048
Nodar Chkuaselidze <nodar.chkuaselidze@gmail.com>
2015-10-26 15:33:20 +0400

    500 symbols, simplify code

--- rain-symbols/main.js
+++ rain-symbols/main.js
@@ -1,29 +1,18 @@
 
 var win = getWindowSize();
 var symbols = generateSymbols('ა', 'ჰ');
-var count = 400;
+var count = 500;
 var bodyEl = document.body;
 
 
 createRandomSpans(count);
 
-window.onresize = function () {
-  console.log(getWindowSize());
-};
-
-function getWindowSize() {
-  return {
-    w : window.innerWidth,
-    h : window.innerHeight
-  };
-}
-
 function createRandomSpans(count) {
   var elems = [];
 
   for (var i = 0; i < count; i++) {
-    var randPos = Math.round(Math.random() * (win.w - 10));
-    var randSym = Math.round(Math.random() * (symbols.length - 1));
+    var randPos = random(0, win.w - 10);
+    var randSym = random(0, symbols.length - 1);
     var span = document.createElement('span');
 
     span.innerHTML = symbols[randSym];
