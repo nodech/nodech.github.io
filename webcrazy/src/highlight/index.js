559b6792c2628defb9892593a09bb1df9f5d6a48
Nodar Chkuaselidze <nodar.chkuaselidze@gmail.com>
2016-07-27 20:15:21 +0400

    Remove webcrazy ??? add numbers

--- webcrazy/src/highlight/index.js
+++
@@ -1,5 +0,0 @@
-var Highlight = require('./highlight');
-var hljs = new Highlight();
-hljs.registerLanguage('javascript', require('./languages/javascript.js'));
-hljs.registerLanguage('qartulskripti', require('./languages/qartulskripti.js'));
-module.exports = hljs;
